94760c0de82889d3ecf03870168c0c71e05c0ffd
Lucas Bracq <lucas.bracq@etu.univ-amu.fr>
2020-02-18 15:55:54 +0100

    debut gagne

---
+++ js/gagne.js
@@ -0,0 +1,14 @@
+(function(){
+    "use strict";
+    $(() => {
+        $.ajax({
+            url: "",
+            method: "post",
+        })
+            .done(function (etat) {
+                if (etat === "gagne"){
+
+                }
+            })
+    })
+})();
