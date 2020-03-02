c7c5a86a48e4c24c65cfd0c4f594a18b19cf457a
hsiacelia <celia.hsia@etu.univ-amu.fr>
2020-03-02 22:51:35 +0100

    fini

--- php/Timer.php
+++ php/Timer.php
@@ -1,54 +1,36 @@
 <?php
-class Timer {
+class Timer
+{
     private $Date;
 
-    function commencer($param){
+    function commencer($debut, $param) // debut c'est le timestamp
+    {
         date_default_timezone_set('Europe/Paris');
 
-        if($param == true) { // timer pour les tours
-            $this->Date = strtotime("+20");
-        }
-        elseif($param == false) { // timer pour le placement des pions
-            $this->Date = strtotime("+60");
+        if ($param == true) { // timer pour les tours
+            $this->Date = $debut + 20; // 20 secondes
+        } elseif ($param == false) { // timer pour le placement des pions
+            $this->Date = $debut + 60; // 60 secondes
         }
     }
 
-    function tour(){
+    function reste()
+    {
         $fin = $this->Date;
         $mtn = strtotime('now');
-        $reste =  $fin - $mtn;
+        $reste = $fin - $mtn;
         return $reste;
     }
 
-    function affiche($nbTour,$param){
-        if (isset($_POST['type']) == true && $_POST['type'] == 'Timer'){
-            $var = $this->tour();
-            echo $var;
-            if($param == true){ // pour les tours
-                if($var > 0){
-                    $arr = array('tour'+$nbTour=>'encours');
-                    json_encode($arr);
-                }
-                else{
-                    $arr = array('tour'+$nbTour=>'kick');
-                    json_encode($arr);
-                    echo 'kick';
-                }
-            }
-            else{ // pour le positionnement des pions
-                if($var > 0){
-                    $arr = array('choix'=>'encours');
-                    json_encode($arr);
-                }
-                else{
-                    $arr = array('choix'=>'kick');
-                    json_encode($arr);
-                    echo 'kick';
-                }
-            }
+    function resultat($debut, $param){
+        $this->commencer($debut, $param);
+        $var = $this->reste();
+        if($var > 0){ // il reste du temps
+            return true;
+        }
+        else{ // il ne reste plus de temps
+            return false;
         }
     }
-
-
 
 }
