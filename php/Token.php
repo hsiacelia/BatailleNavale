118513cde011322b03b18540f7e9f34ceb96c22f
hsiacelia <celia.hsia@etu.univ-amu.fr>
2020-03-04 16:10:36 +0100

    fini

--- php/Token.php
+++ php/Token.php
@@ -1,41 +1,23 @@
 <?php
 
-class Token {
-    private $joueur;
-    //lorsqu'on passe au tour suivant faut reinitialiser le token et pas quand on tire
-    public function __construct($j)
-    {
-        //le json file à partir de joueur
-        $this->joueur = $j;
+function create(){
+    $str = array();
+    $i = 0;
+    while ($i<10) {
+        $str = $str + rand(0,9);
+        ++$i;
     }
+    return implode($str);
+}
 
-    function create(){
-        $str = array();
-        $i = 0;
-        while ($i<5) {
-            $str = $str + rand(0,9);
-            ++$i;
-        }
-        return implode($str);
+function verifier($joueur,$token){
+    if($token == null){
+        $joueur['token'] = $token;
+        return true; // il peut jouer
     }
-
-    function verifier(){
-        //il faut une variable temporaire dans le json pour pas ecraser le token et pouvoir comparer
-        $token = $this->joueur['token'];
-        $tmp = $this->joueur['tmp'];
-        if($token == null){
-            $this->joueur['token'] = $this->create();
-            return true; // il peut jouer
-        }
-        elseif($token != null){
-            if($token == $tmp) {
-                return true; // il peut jouer
-            }
-            else return false; // il ne peut pas jouer il triche !!!
-        }
+    $tokenjs = $joueur['token'];
+    if($tokenjs == $token) {
+        return true; // il peut jouer
     }
-
-    function ajouter(){
-        $this->joueur['tmp'] = $this->create();
-    }
+    return false; // il peut pas jouer il triche !!!
 }
