996c984185ce2489e806b759d04e7c26298ccd72
noe-guyomarch <noe.guyomarch@etu.univ-amu.fr>
2020-03-04 17:32:00 +0100

    ajout

--- json/jouer.php
+++ json/jouer.php
@@ -5,10 +5,12 @@ header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');// pour ne pas garder de cache
 header('Content-type: application/json');
 
 $var = 'bonjour';
+// ouvrir le attente.json et verifier si il y a une partie en attente
 
 $obj = new stdClass;
-$obj ->message = 'mauvais nom d\'utilisateur ou mot de passe';
-$obj ->success = false;
+$obj ->id = 'mauvais nom d\'utilisateur ou mot de passe';
+$obj ->nombreJoueurs = false;
+$obj ->joueurs = 
 
 echo(json_encode($var));
 file_put_contents('nom.json', json_encode($obj));
