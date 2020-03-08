36a05011f59d1c125aa8c9f11842f81e7bcfc7be
noé-guyomarch <49200628+noe-guyomarch@users.noreply.github.com>
2020-03-08 20:13:42 +0100

    ajout du systeme de creation de partie
    reste a tester et corriger

--- js/Menu.js
+++ js/Menu.js
@@ -1,7 +1,7 @@
 
 
-
 //cree les element du menu : (bouton jouer, crédits ...)
+
 class Menu {
     constructor() {
         $('body').append(
@@ -9,6 +9,7 @@ class Menu {
                 .attr('id', 'bouton-jouer')
                 .html('Jouer')
                 .click(function () {
+                    //appel du fichier jouer.php
                     $.ajax({
                         url: './json/jouer.php',
                         method: 'POST'
@@ -22,6 +23,7 @@ class Menu {
                         $('body').html('Problème au niveau du serveur. <br> Le problème est de notre coté veuillez patienter.<br>Si le problème persiste veuillez nous contacter.');
                     })
                 }),
+                
             // div clickable pour les crédit, doit afficher les credits
             $('<div />')
                 .attr('id', 'div-credits')
--- json/jouer.php
+++ json/jouer.php
@@ -4,14 +4,66 @@ header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');// pour ne pas garder de cache
 header('Content-type: application/json');
 
-$var = 'bonjour';
+$obj = new stdClass;
+$nouvellePartie = new stdClass;
+$trouvePartie = FALSE;
+
 // ouvrir le attente.json et verifier si il y a une partie en attente
 
-$obj = new stdClass;
-$obj ->id = 'mauvais nom d\'utilisateur ou mot de passe';
-$obj ->nombreJoueurs = false;
-$obj ->joueurs = 
+$fichierAttente = "attente.json";
+if (file_exists($fichierAttente)) {
 
-echo(json_encode($var));
-file_put_contents('nom.json', json_encode($obj));
+    $json = file_get_contents($fichierAttente);
+
+    $attente = json_decode($json, true);
+    unset($json); // plus necessaire
+
+    $partieEnAttente = $attente['parties'];
+
+    // si une partie est en attente
+    for ($i=0; $i < count($partieEnAttente); ++$i) { 
+        if ($partieEnAttente[$i]['nombreJoueurs'] < 2) {
+            $trouvePartie = TRUE;
+
+            $nomFichier = $partieEnAttente[$i]['id'] . ".json";
+            if (file_exists($nomFichier)) break; // a verifier
+
+            $contenuFichier = $partieEnAttente[$i];
+
+            file_put_contents($nomFichier, json_encode($contenuFichier));
+
+            // on enleve la partie selectionne au tableau
+            unset($attente['parties'][$i]); 
+
+            // remettre le tableau dans attente.json
+            file_put_contents($fichierAttente, json_encode($attente));
+            break; //on quitte la boucle si on trouve un partie en attente
+        }
+    }
+    // si il n y a pas de partie en attente
+    if (!$trouvePartie) {
+        // genere un nom aleatoir et verifie si en utilisation
+        $nomFichier = '';
+        do {
+            for ($i=0; $i < 4; $i++) { 
+                $nomFichier = $nomFichier . (mt_rand(0,9));
+            }
+        } while (file_exists($nomFichier . ".json"));
+        
+        // nouvelle partie a ajoutter dans attente.json
+        $nouvellePartie ->id = $nomFichier;
+        $nouvellePartie ->nombreJoueurs = 1;
+        // $nouvellePartie ->joueurs = ;
+        array_push($attente['parties'], $nouvellePartie);
+        file_put_contents($fichierAttente, json_encode($attente));
+    }
+}
+
+$ok = 'bonjour';
+
+echo(json_encode($ok));
+
+
+
+file_put_contents('nom.json', json_encode($nouvellePartie));
 ?>
