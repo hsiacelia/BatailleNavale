36d0fbc8838c8024c84ad9970c0b5d14aed4eee5
hsiacelia <celia.hsia@etu.univ-amu.fr>
2020-03-20 15:19:40 +0100

    coordonnee du pion

--- js/Pion.js
+++ js/Pion.js
@@ -3,8 +3,6 @@
         let dropper = document.getElementById('dropper');
         dropper.addEventListener('dragover', function(e){
             if(!e.preventDefault()){ // si la case n'est pas vide
-                e.data('x');
-                e.data('y');
                 var requete = new XMLHttpRequest();
                 requete.open('GET', '/PHP/Pion.php?x='+e.data('x')+'?y='+e.data('y'), true);
                 requete.send();
