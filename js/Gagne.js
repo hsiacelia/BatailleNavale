33d1057b7efb761aade7b26b451b035e56878f67
hugo-levet <hugo.levet42130@gmail.com>
2020-03-24 23:35:38 +0100

    première beauté

--- js/Gagne.js
+++ js/Gagne.js
@@ -1,5 +1,14 @@
 class Gagne {
     constructor() {
-        $('body').html('Gagne');
+        $('body').html('<div class="finPartie">' +
+            '<div></div>' +
+            '<div id="bulle"><p>J\'ai gagné !</p></div>' +
+            '<div></div>' +
+            '</div>' +
+            '<div class="finPartie">' +
+            '<div><img src="img/garcon 1 - heureux.png" alt=""></div>' +
+            '<div id="rejouer"><button>Rejouez</button></div>' +
+            '<div><img src="img/garcon 1 - triste.png" alt=""></div>' +
+            '</div>');
     }
 }
