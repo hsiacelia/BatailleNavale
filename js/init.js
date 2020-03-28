08da63efde9a49783193a0918c6e26469ba6d315
hugo-levet <hugo.levet42130@gmail.com>
2020-03-28 21:04:09 +0100

    bon

--- js/init.js
+++ js/init.js
@@ -2,8 +2,8 @@
 // let idJoueur = '1111';
 let idPartie = null;
 let idJoueur = null;
-// let urlAjax = './';
-let urlAjax = 'https://hugolevet.fr/git/BatailleNavale/';
+let urlAjax = './';
+// let urlAjax = 'https://hugolevet.fr/git/BatailleNavale/';
 let Etat = new Menu();
 let etatActuel;
 let pageErreur = 'Problème au niveau du serveur.<br>Le problème est de notre coté veuillez patienter.<br>Si le problème perciste veuillez nous contacter.';
