dc3ebebb3742dc690189acb124b84b1c7465dcaa
hugo-levet <hugo.levet42130@gmail.com>
2020-03-31 17:57:40 +0200

    tir header

--- json/tir.php
+++ json/tir.php
@@ -1,8 +1,8 @@
 <?php
 
-// header('Cache-Control: no-cache, must-revalidate');
-// header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-// header('Content-type: application/json');
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-type: application/json');
 
 $obj = new stdClass();
 
